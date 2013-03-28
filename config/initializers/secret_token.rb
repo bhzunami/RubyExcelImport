9b51327c71121e903fe63c69311a9c21e69e4985
bhzunami <nmauchle@gmail.com>
2013-03-28 09:18:39 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyExcelImport::Application.config.secret_token = '0409345c5d75987a49e45a85c158608b6356c5073b93aa9a1baeb2646fb0aa29f398d3bd98656607447457d71b20e31d326ed6e963df52f97429a30700fd0b87'

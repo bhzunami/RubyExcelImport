fb78435c718493400d1a0ef5ee220fe4c5be2f61
Sebastian Widmer <widmer.sebastian@gmail.com>
2013-03-28 14:25:30 +0100

    annotate Trick

--- app/models/trick.rb
+++ app/models/trick.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+# Schema version: 20130328085729
+#
+# Table name: tricks
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  problem    :text
+#  solution   :text
+#  author     :string(255)
+#  solved_by  :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Trick < ActiveRecord::Base
   attr_accessible :author, :problem, :solution, :solved_by, :title
 end
